3d1d82b4ff0f194c00be25713a824fb211a8133b
lucky1551 <luckybagdi1111@gmail.com>
2023-06-01 16:39:21 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import {useState, useRef } from 'react';
-import './app.css';
+
 
 function App() {
    const [input, state] = useState('');
-   const [second,setSecond] = useState();
+   const [second,setSecond] = useState(0);
   
    const render = useRef(0);
    const timeRef = useRef();
-
+   const inputRef = useRef();
    const handleChange = (event) => { 
     state(event.target.value);
     render.current++;
@@ -19,28 +19,40 @@ function App() {
         render.current++;
         setSecond(prev=>prev+1);
   }, 1000);
+  inputRef.current.focus();
+}; 
   const stopTimer = () => {
     clearInterval(timeRef.current);
     timeRef.current = 0;
+    inputRef.current.focus();
   };
   const resetTimer =() => {
     stopTimer();
-       if(second) {render.current++;
-        setSecond(0);}
+       if(second) {
+        render.current++;
+        setSecond(0);
+      }
+      inputRef.current.focus();
       };
   return (
       <div className="App" >
-        <input style={{width: '30vw',height:'10vh',textAlign: 'center',background:'#F16A8A', color:'yellow', marginLeft:'32vw', marginTop:'35vh', fontSize:'2vw' }}
+        <input 
+         ref={inputRef}
         type="text"
         value ={input}
         placeholder="what's the time"
          onChange={handleChange}/>
          <p style={{fontSize:'2vw', textAlign:'center' }}>Number of key pressed: {render.current}</p>
          <br></br>
-         <button style={{width: '30vw',height:'10vh',textAlign: 'center',background:'yellow', color:'black', marginLeft:'32vw', marginTop:'35vh',fontSize:'2vw', cursor:'pointer'}} onClick={startTimer}>Start</button>
+         <div className='timebuttons'>
+         <button onClick={startTimer}>Start</button>
          <button onClick={stopTimer}>Stop</button>
-         <button  onClick={resetTimer}>Reset</button>
+         <button onClick={resetTimer}>Reset</button>
+         </div>
+        
         <br></br>
+        <p>Seconds: {second}</p>
+        <p>Input: {input}</p>
         
       </div>
   );
