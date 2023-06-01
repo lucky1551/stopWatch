ab99a2a6c0938bca88ebe843c167818abb82af86
lucky1551 <luckybagdi1111@gmail.com>
2023-06-01 15:18:55 +0530

    stop watch first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useRef } from 'react';
+import './app.css';
 
 function App() {
+   const [input, state] = useState('');
+   const [second,setSecond] = useState();
+  
+   const render = useRef(0);
+   const timeRef = useRef();
+
+   const handleChange = (event) => { 
+    state(event.target.value);
+    render.current++;
+  }; 
+  
+
+   const startTimer = () => {
+    timeRef.current = setInterval(() => {
+        render.current++;
+        setSecond(prev=>prev+1);
+  }, 1000);
+  const stopTimer = () => {
+    clearInterval(timeRef.current);
+    timeRef.current = 0;
+  };
+  const resetTimer =() => {
+    stopTimer();
+       if(second) {render.current++;
+        setSecond(0);}
+      };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App" >
+        <input style={{width: '30vw',height:'10vh',textAlign: 'center',background:'#F16A8A', color:'yellow', marginLeft:'32vw', marginTop:'35vh', fontSize:'2vw' }}
+        type="text"
+        value ={input}
+        placeholder="what's the time"
+         onChange={handleChange}/>
+         <p style={{fontSize:'2vw', textAlign:'center' }}>Number of key pressed: {render.current}</p>
+         <br></br>
+         <button style={{width: '30vw',height:'10vh',textAlign: 'center',background:'yellow', color:'black', marginLeft:'32vw', marginTop:'35vh',fontSize:'2vw', cursor:'pointer'}} onClick={startTimer}>Start</button>
+         <button onClick={stopTimer}>Stop</button>
+         <button  onClick={resetTimer}>Reset</button>
+        <br></br>
+        
+      </div>
   );
 }
 
